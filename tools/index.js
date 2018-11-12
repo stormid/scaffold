ca3bf5dea903571a82542132305e45209aebf80c
Mick Perkins <mick.perkins@stormid.com>
2018-11-12 08:49:18 +0000

    Adds fs-extra to create dir before write

--- tools/index.js
+++ tools/index.js
@@ -1,7 +1,7 @@
 import { h, doctype } from './dom';
 import { walker } from '../tools/utils';
 import render from 'preact-render-to-string';
-import fs from 'fs';
+import fse from 'fs-extra';
 import path from 'path';
 
 const pages = walker(__dirname, `../src/html/pages`);
@@ -9,7 +9,7 @@ const pages = walker(__dirname, `../src/html/pages`);
 for(let page of pages){
     const html = render(require(`../src/html/pages/${page.path}/${page.name}`).default());
     //to do - mkdir if it doesn't exist
-    fs.writeFile(
+    fse.outputFile(
         path.resolve(__dirname, path.join(`../build`, page.path, `${page.name.replace(/.js/, '')}.html`)),
         doctype(html), 'utf8',
         () => {}
