0ee58dedde7210e71ce513fdf8ffef3690a65082
Mick Perkins <mickperkins@gmail.com>
2018-11-20 20:51:09 +0000

    Adds async rendering example with node-fetch

--- src/html/pages/dashboard.js
+++ src/html/pages/dashboard.js
@@ -1,17 +1,16 @@
 import { h, ghost } from '../../../tools/dom';
 import DefaultLayout from '../layouts/default';
 import Card from '../components/card';
-import { xkcd } from '../../data';
+import { resource } from '../../data';
 
-const DashboardPage = () => xkcd().then(res => res.json()).then(data => new Promise(resolve => {
-    console.log(data);
+const DashboardPage = () => resource().then(res => res.json()).then(data => new Promise(resolve => {
     resolve(<DefaultLayout title={'Dashboard'}>
         <div class="card__list">
         {
-            data.map(strip => <Card
-                href={strip.img}
-                title={strip.title}
-                summary={strip.alt}
+            data.title.map(datum => <Card
+                href={datum['@uri']}
+                title={`${datum.titleweb}, ${datum.authorweb}`}
+                // summary={datum.flapcopy}
             />)
         }
         </div>
