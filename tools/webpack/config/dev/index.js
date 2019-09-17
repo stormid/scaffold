b393719a2b9a4f44372b6d48cdcf0d96a3d75cf9
Mick Perkins <mick.perkins@stormid.com>
2019-09-17 11:02:23 +0100

    Adds devServer

--- tools/webpack/config/dev/index.js
+++ tools/webpack/config/dev/index.js
@@ -14,6 +14,7 @@ module.exports = [
         output: {
             filename: '[name].js',
             path: path.join(process.cwd(), paths.output),
+            publicPath: '/',
             libraryTarget: `umd`
         },
         mode: 'development',
