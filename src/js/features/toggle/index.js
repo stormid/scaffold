e4833c725374ccc47d5adc2585916d423b358381
Mick Perkins <mickperkins@gmail.com>
2019-10-05 16:01:14 +0100

    Removes js/features dir

--- src/js/features/toggle/index.js
+++
@@ -1,7 +0,0 @@
-import { TOGGLE } from '../../constants';
-import Toggle from './lib';
-
-export default () => {
-    document.querySelector(TOGGLE.SELECTOR.GLOBAL) && Toggle.init(TOGGLE.SELECTOR.GLOBAL);
-    document.querySelector(TOGGLE.SELECTOR.LOCAL) && Toggle.init(TOGGLE.SELECTOR.LOCAL, TOGGLE.OPTIONS.LOCAL);
-};
