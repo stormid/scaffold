f0581c0ad3577b82b1cfa57d772a1f2b3b6e70ec
Mick Perkins <mick.perkins@stormid.com>
2019-10-14 17:49:40 +0100

    Adds title to HTML pipeline, #33

--- src/templates/pages/index.js
+++ src/templates/pages/index.js
@@ -1,6 +1,8 @@
 import { h } from 'preact';
 import DefaultLayout from '../layouts/default';
 
+export const title = 'Home';
+
 const HomePage = () => <DefaultLayout>
     
 </DefaultLayout>;
--- tools/webpack/plugins/static-entry.js
+++ tools/webpack/plugins/static-entry.js
@@ -6,15 +6,16 @@ require('../../../src/css/index.scss');
 module.exports = function(locals) {
 	const body = require(`../../../src/templates/pages/${locals.path}`).default();
 	const assets = Object.keys(locals.webpackStats.compilation.assets);
-	const css = assets.filter(value => value.match(/\.css$/));
+	const css = assets.filter(value => value.match(/\.css$/));	
+	const title = require(`../../../src/templates/pages/${locals.path}`).title || '';
 	try {
 		return new Promise((resolve, reject) => {
 			if(body.then) {
 				body.then(Res => {
-					resolve(`<!DOCTYPE html>${render(<Html css={css} htmlBody={Res} />)}`);
+					resolve(`<!DOCTYPE html>${render(<Html css={css} htmlBody={Res} title={title} />)}`);
 				});
 			}
-			else resolve(`<!DOCTYPE html>${render(<Html css={css} htmlBody={body} />)}`);
+			else resolve(`<!DOCTYPE html>${render(<Html css={css} htmlBody={body} title={title} />)}`);
 		});
 	} catch(e){
 		console.log(`HTML render error: ${e}`);
