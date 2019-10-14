bac912a5a55f479d2268bb4e78e147bdea3ebf42
Mick Perkins <mick.perkins@stormid.com>
2019-10-14 17:50:11 +0100

    Adds title to HTML pipeline, #33 (#34)

--- src/templates/pages/grid.js
+++ src/templates/pages/grid.js
@@ -1,5 +1,7 @@
 import { h } from 'preact';
 
+export const title = 'Grid';
+
 const Grid = () => <body>
     <main id="main">
         <div class="wrap">
--- tools/webpack/plugins/default-html.js
+++ tools/webpack/plugins/default-html.js
@@ -1,13 +1,14 @@
 const h = require('preact').h;
 const paths = require('../../../paths.config');
 
-module.exports = function({ htmlBody, css }) {
+module.exports = function({ htmlBody, css, title }) {
 	return (
 		<html>
 	  		<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
 				<link rel="shortcut icon" href={`/${paths.dest.img}/favicon.ico`} />
+				<title>{title}</title>
 				{css && <link rel="stylesheet" href={`/${css}`} />}
 				<script src={`${process.env.NODE_ENV === 'production' ? `/${paths.dest.js}` : ''}/index.js`} async></script>
 			</head>
