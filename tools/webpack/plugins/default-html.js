3bababd90de3c1749c2bd122dce61ea79eae002b
Mick Perkins <mick.perkins@stormid.com>
2019-10-14 12:29:44 +0100

    Updates default html template

--- tools/webpack/plugins/default-html.js
+++ tools/webpack/plugins/default-html.js
@@ -2,18 +2,16 @@ const h = require('preact').h;
 const paths = require('../../../paths.config');
 
 module.exports = function({ htmlBody, css }) {
-  return (
-    <html>
-      <head>
-      <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
-        <link rel="shortcut icon" href={`/${paths.dest.img}/favicon.ico`} />
-        {css && <link rel="stylesheet" href={`/${css}`} />}
-        <script src={`${process.env.NODE_ENV === 'production' ? `/${paths.dest.js}` : ''}/index.js`} async></script>
-      </head>
-      <body>     
-        {htmlBody}
-      </body>
-    </html>
+	return (
+		<html>
+	  		<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+				<link rel="shortcut icon" href={`/${paths.dest.img}/favicon.ico`} />
+				{css && <link rel="stylesheet" href={`/${css}`} />}
+				<script src={`${process.env.NODE_ENV === 'production' ? `/${paths.dest.js}` : ''}/index.js`} async></script>
+			</head>
+			{htmlBody}
+	</html>
   )
 };
