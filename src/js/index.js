d9f363544769a1e5644edc275d719e5ada7a83b8
Jonathon Turnbull <jonathon.turnbull@stormid.com>
2019-10-31 16:11:46 +0000

    fixed a couple of linting issues

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,19 @@
 import Toggle from './modules/toggle';
 
-export const initStack = [
+const initStack = [
   Toggle,
-  // Importer(`tabs`)
-  // () => { import(/* webpackChunkName: "toggle" */`./features/toggle`).then(module => module.default()); },
-  // () => { import(/* webpackChunkName: "validate" */`@stormid/validate`).then(module => module.default.init(VALIDATE.SELECTOR)); },
-  // Importer.bind(null, 'validate')(import(/* webpackChunkName: "validate" */`@stormid/validate`)),
+  // Importer(`tabs`),
+  // () => {
+  //   import(/* webpackChunkName: "toggle" */ `./features/toggle`)
+  //     .then(module => module.default());
+  // },
+  // () => {
+  //   import(/* webpackChunkName: "validate" */ `@stormid/validate`)
+  //     .then(module => module.default.init(VALIDATE.SELECTOR));
+  // },
+  // Importer.bind(null, 'validate')(import(/* webpackChunkName: "validate" */ `@stormid/validate`)),
 ];
+
+export default {
+  initStack,
+};
--- src/js/modules/toggle/index.js
+++ src/js/modules/toggle/index.js
@@ -2,6 +2,11 @@ import { TOGGLE } from '../../constants';
 import Toggle from './lib';
 
 export default () => {
-  document.querySelector(TOGGLE.SELECTOR.GLOBAL) && Toggle.init(TOGGLE.SELECTOR.GLOBAL);
-  document.querySelector(TOGGLE.SELECTOR.LOCAL) && Toggle.init(TOGGLE.SELECTOR.LOCAL, TOGGLE.OPTIONS.LOCAL);
+  if (document.querySelector(TOGGLE.SELECTOR.GLOBAL)) {
+    Toggle.init(TOGGLE.SELECTOR.GLOBAL);
+  }
+
+  if (document.querySelector(TOGGLE.SELECTOR.LOCAL)) {
+    Toggle.init(TOGGLE.SELECTOR.LOCAL, TOGGLE.OPTIONS.LOCAL);
+  }
 };
