872e306d58bf580b2f6f1b1ccb04700ed30f75be
Mick Perkins <mick.perkins@stormid.com>
2020-01-07 21:21:35 +0000

    Adds linting using @stormid/eslint-config; formatting fixes, re #4 (#42)

--- src/templates/components/skip/index.js
+++ src/templates/components/skip/index.js
@@ -1,6 +1,6 @@
 import { h } from 'preact';
 
-const Skip = ({ 
+const Skip = ({
     href = '#main',
     label = 'Skip to main content'
 }) => <div class="skip"><a class="skip__btn" href={href}>{label}</a></div>;
--- src/templates/pages/grid.js
+++ src/templates/pages/grid.js
@@ -6,8 +6,8 @@ const Grid = () => <body>
     <main id="main">
         <div class="wrap">
             {
-                Array.apply(null, Array(12)).map((v, i) => <div class="row">
-                    { Array.apply(null, Array(Math.floor(12 / (i + 1)))).map((v, j) => <div class={`col xs-${i + 1} sm-${i + 1} md-${i + 1} lg-${i + 1}`} style={'height:30px;margin-bottom:24px;background: rgba(0,0,0, .1)'}></div>)}
+                Array.apply({}, Array(12)).map((v, i) => <div class="row">
+                    { Array.apply({}, Array(Math.floor(12 / (i + 1)))).map((v, j) => <div class={`col xs-${i + 1} sm-${i + 1} md-${i + 1} lg-${i + 1}`} style={'height:30px;margin-bottom:24px;background: rgba(0,0,0, .1)'} />)}
                 </div>)
             }
         </div>
