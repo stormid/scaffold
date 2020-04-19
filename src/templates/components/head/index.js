49acedff7e54ce7d29aa97769babe332c15fdc70
Mick Perkins <mick.perkins@stormid.com>
2020-04-19 16:44:46 +0100

    Adds head component and support for per-page meta tags, re #46 (#51)

---
+++ src/templates/components/head/index.js
@@ -0,0 +1,16 @@
+import { h } from 'preact';
+
+const head = ({ title, meta, children }) => <head>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+    <link rel="shortcut icon" href={`/static/img/favicon.ico`} />
+    <title>{title}</title>
+    {children}
+    {meta && meta.map(item => {
+        if (item.name) return <meta name={item.name} content={item.content} />;
+        if (item.property) return <meta property={item.property} content={item.content} />;
+    })}
+</head>;
+
+export default head;
+    
