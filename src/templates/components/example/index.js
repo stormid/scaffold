662cc8e39db23276389619f3b66775f98020204d
Mick Perkins <mick.perkins@stormid.com>
2021-11-05 11:43:53 +0000

    Update Storybook (#92)
    Add docs page example
    
    Add placeholder introduction docspage

--- src/templates/components/example/index.js
+++ src/templates/components/example/index.js
@@ -1,14 +1,14 @@
 import { h } from 'preact';
 
-export const Example = ({ href, title, summary, children }) => <a href={href} class="example">
+export const Example = ({ href, title, summary, children }) => <div class="example">
     <div class="example__bd">
-    { title && <h1 class="example__title">{title}</h1> }
-    { summary && <div class="example__summary">{summary}</div> }
-    { children }
+        <h1 class="example__title"><a href={href} class="example__link">{title}</a></h1>
+        { summary && <div class="example__summary">{summary}</div> }
+        { children }
     </div>
-</a>;
+</div>;
 
-export const Ghost = () => <div class="example ghost">
+export const Ghost = () => <div class="example example--ghost">
     <div class="example__bd">
         <div class="example__title" />
         <div class="example__summary" />
@@ -17,6 +17,6 @@ export const Ghost = () => <div class="example ghost">
 
 export const Empty = () => <div class="example example--empty" />;
 
-export const Error = () => <div class="example example--error" />;
+export const Error = () => <div class="example example--error">Something went wrong :(</div>;
 
 export default Example;
