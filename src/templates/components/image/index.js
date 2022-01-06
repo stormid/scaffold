c6862f9834347588ede22186da509f7db9af5bda
Nick Watton <92303959+Nick-StormID@users.noreply.github.com>
2022-01-06 16:00:41 +0000

    Feature/image component (#97)
    * Initial Image component developement
    
    * v2 Image component
    
    * v2 Figure
    
    * Accessibility example
    
    * v3 BEM and light refactor
    
    * Removed unused versions and assets. Moved notes to DOCS file
    
    * Removed redundant ternaries. Added separate class for caption
    
    * #98 - Cleaned up per PR comments

---
+++ src/templates/components/image/index.js
@@ -0,0 +1,63 @@
+import { h } from 'preact';
+
+/**
+ * Responsive image component, using the <picture> element
+ * @param {string} alt - Image alt tag
+ * @param {string} decoding='async' - 'async', 'sync', 'auto'
+ * @param {string} loading='lazy' - 'lazy', 'eager'
+ * @param {string} src - Reference to local/relative or remote image URI
+ * @param {string} className - Class name
+ * @param {array} [sources]	- Array of Objects for <picture> —> <source> attributes {src, media}
+ **/
+const Image=({
+	className,
+	alt,
+	src,
+	sources,
+	decoding='async',
+	loading='lazy'
+	}) =>
+
+<picture class={className} >
+
+	{
+		sources && sources.map(srcData => <source srcset={srcData.src} media={srcData.media} />)
+	}
+
+	<img 
+		alt={alt}
+		class={className}
+		decoding={decoding}
+		src={`${src}`}
+		loading={loading}
+	/>
+
+</picture>
+export default Image;
+
+/**
+ * Wrapper for ImageBlock component, usually where a caption is required
+ * @param {Object} children - an ImageBlock 
+ * @param {string} caption - Text to display with image
+ * @param {string} className - Class name for the <figure>
+ * @param {string} captionClassName - Class name  for the <caption>
+ */
+export const Figure = ({
+	children,
+	className,
+	captionClassName,
+	caption}) =>
+
+<figure class={className} >
+
+	{
+		children
+	}
+	
+	{caption && 
+		<figcaption class={captionClassName}>
+			{caption}
+		</figcaption>
+	}
+	
+</figure>
