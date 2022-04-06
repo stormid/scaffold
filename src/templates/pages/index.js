99a79697c43a1f3cb5dfa125a789791cf657964b
Mick Perkins <mick.perkins@stormid.com>
2022-04-06 12:38:28 +0100

    Clean-up

--- src/templates/pages/index.js
+++ src/templates/pages/index.js
@@ -8,23 +8,6 @@ export const title = 'Home';
 //     content: ''
 // }];
 
-const HomePage = () => <DefaultLayout section={'Home'}>
-    <figure>
-        <img src="/static/img/test-jpg.jpg" />
-        <figcaption>jpg</figcaption>
-    </figure>
-    <figure>
-        <img src="/static/img/test-jpg.webp" />
-        <figcaption>jpg -&gt; webp</figcaption>
-    </figure>
-    <figure>
-     <img src="/static/img/test-png.png" />
-        <figcaption>png</figcaption>
-    </figure>
-    <figure>
-        <img src="/static/img/test-png.webp" />
-        <figcaption>jpg -&gt; png</figcaption>
-    </figure>
-</DefaultLayout>;
+const HomePage = () => <DefaultLayout />;
 
 export default HomePage;
