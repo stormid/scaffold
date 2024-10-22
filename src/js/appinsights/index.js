23e9a3ca2f899eda815981cd0076783b9461dbb3
Sarah Richards <62299569+sarah-storm@users.noreply.github.com>
2024-10-22 17:26:35 +0100

    Update to avoid return outside of function (#159)

--- src/js/appinsights/index.js
+++ src/js/appinsights/index.js
@@ -1,8 +1,7 @@
 import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 
-{
-    const connectionString = document.querySelector(`[data-ai]`)?.getAttribute('data-ai');
-    if (!connectionString) return;
+const connectionString = document.querySelector(`[data-ai]`)?.getAttribute('data-ai');
+if (connectionString) {
     const appInsights = new ApplicationInsights({ config: { connectionString } });
     appInsights.loadAppInsights();
     appInsights.trackPageView();
