adb16a4e534d5a6d1fbe84b04ac836128bb84128
Mick Perkins <mick.perkins@stormid.com>
2024-10-29 10:26:01 +0000

    Add example

--- src/templates/pages/index.js
+++ src/templates/pages/index.js
@@ -1,6 +1,6 @@
 import { h } from 'preact';
 import DefaultLayout from '@layouts/default';
-// import Example, { Ghost, Empty, Error } from '@components/example';
+import Example, { Ghost, Empty, Error } from '@components/example';
 
 export const title = 'Home';
 
@@ -12,12 +12,12 @@ export const title = 'Home';
 const HomePage = () => <DefaultLayout>
     <div class="wrap">
         <h1>Hello world.</h1>
-        {/* <Ghost />
+        <Ghost />
         <Empty />
         <Error />
         <Example href={'#'}
             title={'Quick brown fox'}
-            summary={'Jumps over the lazy dog'} /> */}
+            summary={'Jumps over the lazy dog'} />
     </div>
 </DefaultLayout>;
 
